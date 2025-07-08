f3d07308d53161a773ed9265bd8a351938c431c8
Harsh Sahu <111741242+Harshusah2@users.noreply.github.com>
2025-07-08 21:05:30 +0530

    Show the generated interviews on the homepage

--- app/(Homeroot)/page.tsx
+++ app/(Homeroot)/page.tsx
@@ -1,11 +1,26 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
-import { dummyInterviews } from '@/constants'
+import { dummyInterviews, interviewer } from '@/constants'
+import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+
+  const user = await getCurrentUser();
+
+  // for optimization, we can use Promise.all to fetch both user interviews and latest interviews concurrently at the same time
+  // This will reduce the overall time taken to fetch both data sets
+  // If we don't use Promise.all, it will wait for the first query to finish before starting the second query
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id! })
+  ]);
+  
+  const hasPastInterviews =  userInterviews?.length > 0;
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
+
   return (
     <>
       <section className='card-cta'>
@@ -28,11 +43,15 @@ const page = () => {
         <h2>Your Interviews</h2>
 
         <div className='interviews-section'>
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-
-          {/* <p>You haven't taken any interviews yet</p> */}
+          {
+            hasPastInterviews ? (
+              userInterviews?.map((interview) => (
+                <InterviewCard {...interview} key={interview.id} />
+              ))
+            ) : (
+              <p>You haven't taken any interviews yet</p>
+            )
+          }
         </div>
       </section>
 
@@ -40,9 +59,15 @@ const page = () => {
         <h2>Take an Interview</h2>
 
         <div className='interviews-section'>
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {
+            hasUpcomingInterviews ? (
+              latestInterviews?.map((interview) => (
+                <InterviewCard {...interview} key={interview.id} />
+              ))
+            ) : (
+              <p>There are no new interviews available</p>
+            )
+          }
         </div>
       </section>
     </>
